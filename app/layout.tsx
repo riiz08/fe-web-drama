75246bf63297aae2ceb0a26c3957cc8cb3ea8368
riiz08 <mhmdrizki0812@gmail.com>
2025-04-14 12:12:57 +0700

    hehhehee

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,15 +1,10 @@
 import "@/styles/globals.css";
 import { Metadata, Viewport } from "next";
-import { Link } from "@heroui/link";
 import clsx from "clsx";
-
 import { Providers } from "./providers";
-
 import { siteConfig } from "@/config/site";
 import { fontSans } from "@/config/fonts";
-import { Navbar } from "@/components/navbar";
-import AdsterraAntiAdblock from "@/components/AdsterraAntiAdblock";
-import AdsterraNative from "@/components/AdsterraNative";
+import AdsWrapper from "@/components/AdsWrapper";
 
 export const metadata: Metadata = {
   title: {
@@ -46,25 +41,7 @@ export default function RootLayout({
         )}
       >
         <Providers themeProps={{ attribute: "class", defaultTheme: "dark" }}>
-          <div className="relative flex flex-col h-screen">
-            <Navbar />
-            <main className="container mx-auto max-w-7xl  px-6 flex-grow">
-              {children}
-            </main>
-            <AdsterraAntiAdblock />
-            <AdsterraNative />
-            <footer className="w-full flex items-center justify-center py-3">
-              <Link
-                isExternal
-                className="flex items-center gap-1 text-current"
-                href="https://github.com/riiz08"
-                title="Github Riiz"
-              >
-                <span className="text-default-600">Powered by</span>
-                <p className="text-primary">MangEakkk</p>
-              </Link>
-            </footer>
-          </div>
+          <AdsWrapper>{children}</AdsWrapper>
         </Providers>
       </body>
     </html>
